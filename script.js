aa1b217461835b6e21d6ef2749ee45f25e64c5e2
Subham Roy <83629415+itssubhamroy23@users.noreply.github.com>
2023-04-29 20:55:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=a8c19d53-1e77-4d72-8ae5-b43ec49cb7c8&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success") return;
+
+            const matchesList = data.data;
+
+            if (!matchesList) return [];
+
+            //add your api key from cricketdata.org
+            const relevantData = matchesList.filter(match => match.series_id == "c75f8952-74d4-416f-b7b4-7da4b4e3ae6e").map(match => `${match.name}, ${match.status}`);
+
+            console.log({ relevantData });
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
